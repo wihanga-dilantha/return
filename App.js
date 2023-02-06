1f412467ab485835edfcafd5bb4cba8906050f26
wihanga <wihangaict123@gmail.com>
2023-02-06 14:03:27 +0530

    Update App.js
    add text return to the center

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This app is working properly</Text>
+      <Text style={styles.header}>Return</Text>
       <Text>published successfully</Text>
       <StatusBar style="auto" />
     </View>
@@ -18,4 +18,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    fontSize:30,
+    fontWeight: '800',
+    fontFamily: 'notoserif',
+    color: '#A070CD'
+  }
 });
